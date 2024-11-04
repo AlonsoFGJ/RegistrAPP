f908cf32bdc485ef51992fa5d729bbacfa438e51
AlonsoFGJ <alo.gonzalezj@duocuc.cl>
2024-11-04 16:53:27 -0300

    Version 8 (PDF y diseño de "Olvidar contraseña"

--- src/app/pages/informes/informes.page.ts
+++ src/app/pages/informes/informes.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ApiService } from 'src/app/servicios/api.service';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-informes',
@@ -218,6 +220,25 @@ export class InformesPage implements OnInit {
 
   btncerrar(){
     this.navCtrl.navigateBack(['/login'])
+  }
+
+  descargarPDF() {
+    const paghtml = document.getElementById('informe');
+
+    if (paghtml) {
+      html2canvas(paghtml).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        
+        const imgWidth = 210; 
+        // const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const imgHeight = 430;
+        const position = 0;
+
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save('informe.pdf'); //Nombre del PDF
+      });
+    }
   }
 
 }
